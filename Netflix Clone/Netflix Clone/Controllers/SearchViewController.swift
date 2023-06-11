63164c1afe2c7f72d2f97ea810d3e50b0fc6e8bd
Elizeu RS <13ders@gmail.com>
2023-06-11 20:36:37 -0300

    create initial setup

---
+++ Netflix Clone/Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Elizeu RS on 11/06/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//      view.backgroundColor = .systemPink
+      view.backgroundColor = .systemBackground
+
+      
+    }
+
+}
---
+++ Netflix Clone/Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Elizeu RS on 11/06/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//      view.backgroundColor = .systemGreen
+      view.backgroundColor = .systemBackground
+
+      
+    }
+
+}
