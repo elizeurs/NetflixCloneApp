f7123dc4d1538976b408b85a23e3e938687ad4f1
Elizeu RS <13ders@gmail.com>
2023-06-12 14:41:14 -0300

    create TableView on HomeViewController

--- Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -8,14 +8,61 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-//      view.backgroundColor = .systemRed
-      view.backgroundColor = .systemBackground
-
-
-    }
-
+  
+  private let homefeedTable: UITableView = {
+    let table = UITableView(frame: .zero, style: .grouped)
+    table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+    return table
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //      view.backgroundColor = .systemRed
+    view.backgroundColor = .systemBackground
+    view.addSubview(homefeedTable)
+    
+    homefeedTable.delegate = self
+    homefeedTable.dataSource = self
+    
+    homefeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    homefeedTable.frame = view.bounds
+  }
 }
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 20
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//    cell.textLabel?.text = "Hello world"
+    
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+      return UITableViewCell()
+      
+    }
+    
+    cell.backgroundColor = .red
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 200
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 40
+  }
+}
+
