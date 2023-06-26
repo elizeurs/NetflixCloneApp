917c7b7f7c9ef5fc21a06fdc5d07ba0f9e159330
Elizeu RS <13ders@gmail.com>
2023-06-26 15:13:20 -0300

    Hooking things together

--- Netflix Clone/Netflix Clone/Controllers/Core/HomeViewController.swift
+++ Netflix Clone/Netflix Clone/Controllers/Core/HomeViewController.swift
@@ -17,6 +17,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
   
+  private var randomTrendingMovie: Title?
+  private var headerView: HeroHeaderUIView?
+  
   let sectionTitles: [String] = ["Trending  Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
   
   private let homefeedTable: UITableView = {
@@ -37,8 +40,10 @@ class HomeViewController: UIViewController {
     
     configureNavBar()
     
-    let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+    headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
     homefeedTable.tableHeaderView = headerView
+    
+    configureHeroHeaderView()
         
 //    APICaller.shared.getMovie(with: "Harry potter")
 //    APICaller.shared.getMovie(with: "Harry potter") { result in
@@ -47,6 +52,22 @@ class HomeViewController: UIViewController {
     
     //    getTrendingMovies()
 //    fetchData()
+  }
+  
+  private func configureHeroHeaderView() {
+    
+    APICaller.shared.getTrendingMovies { [weak self] result in
+      switch result {
+      case .success(let titles):
+        let selectedTitle = titles.randomElement()
+        
+        self?.randomTrendingMovie = selectedTitle
+        self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? "" ))
+        
+      case .failure(let error):
+        print(error.localizedDescription)
+      }
+    }
   }
   
   private func configureNavBar() {
